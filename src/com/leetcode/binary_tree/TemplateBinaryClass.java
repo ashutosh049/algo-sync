1ba40ec88a544bf43a7eb0f5ddb448f673c6ea75
ashutosh049 <ashutosh049@outlook.com>
2021-12-19 01:12:30 +0530

    A template class used for leetcode problems

---
+++ src/com/leetcode/binary_tree/TemplateBinaryClass.java
@@ -0,0 +1,32 @@
+package com.leetcode.binary_tree;
+
+import static com.util.TreeNode.buildFromNodes;
+
+import com.util.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given the root of a binary tree, return all root-to-leaf paths in any order.
+ * <p><pre>
+ *
+ * </pre> <p>
+ * Example 1
+ *
+ * <pre>
+ *
+ *
+ * </pre> <p>
+ *
+ * @see <a href="> </a>
+ */
+public class TemplateBinaryClass {
+
+  public static void main(String[] args) {
+    Integer[] nodes = new Integer[]{};
+    TreeNode root = buildFromNodes(nodes);
+  }
+
+  // your logic/solution goes here
+}
